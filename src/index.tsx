bdc428ea50db77b14c60fa22c499260e0580040d
xharris <xhh950@gmail.com>
2022-08-15 11:54:44 -0400

    firebase sw?

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-// serviceWorkerRegistration.register({
-//   file: 'firebase-messaging-sw.js',
-// })
+serviceWorkerRegistration.register({
+  file: 'firebase-messaging-sw.js',
+})
