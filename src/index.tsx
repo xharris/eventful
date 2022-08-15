ec113db393b2d76d88060c68c6e9c07f91e1afc3
xharris <xhh950@gmail.com>
2022-08-15 11:49:12 -0400

    firebase sw?

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
@@ -15,3 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+// serviceWorkerRegistration.register({
+//   file: 'firebase-messaging-sw.js',
+// })
