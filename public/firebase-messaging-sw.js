4a1dd5ce6c117b932523405ce74968f567cb9082
xharris <xhh950@gmail.com>
2022-08-15 01:31:39 -0400

    firebase sw?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
-// import { initializeApp } from 'firebase/app'"
-// import { getMessaging } from 'firebase/messaging/sw'"
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
+import { initializeApp } from 'firebase/app'
+import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw'
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js')
+// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
 
-firebase.initializeApp({
+const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'eventful-870ba.firebaseapp.com',
   projectId: 'eventful-870ba',
@@ -12,15 +12,14 @@ firebase.initializeApp({
   appId: '1:79944665764:web:cc722d5d8f9ca080bfb431',
 })
 
-const messaging = firebase.messaging()
+const messaging = getMessaging(app)
 
-messaging.onBackgroundMessage(function (payload) {
+onBackgroundMessage(messaging, (payload) => {
   console.log('Received background message ', payload)
   // Customize notification here
   const notificationTitle = payload.notification.title
   const notificationOptions = {
     body: payload.notification.body,
   }
-
   self.registration.showNotification(notificationTitle, notificationOptions)
 })
