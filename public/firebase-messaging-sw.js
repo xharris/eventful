b11c8ab9a58b1a82f2b6a106382235366f40e3d6
xharris <xhh950@gmail.com>
2022-08-15 01:22:30 -0400

    firebase sw?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,3 +13,14 @@ firebase.initializeApp({
 })
 
 const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload)
+  // Customize notification here
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body,
+  }
+
+  self.registration.showNotification(notificationTitle, notificationOptions)
+})
