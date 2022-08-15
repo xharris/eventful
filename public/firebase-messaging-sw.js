b3f2acf8ef64abd2edc40bb46ab9b7071579bca7
xharris <xhh950@gmail.com>
2022-08-15 10:46:54 -0400

    firebase sw?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,31 @@
-import { initializeApp } from 'firebase/app'
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw'
-// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js')
-// importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
+// import { initializeApp } from 'firebase/app'
+// import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw'
 
-const app = initializeApp({
+// const app = initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   appId: '1:79944665764:web:cc722d5d8f9ca080bfb431',
+//   projectId: 'eventful-870ba',
+//   authDomain: 'eventful-870ba.firebaseapp.com',
+//   storageBucket: 'eventful-870ba.appspot.com',
+//   messagingSenderId: '79944665764',
+// })
+
+// const messaging = getMessaging(app)
+
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log('Received background message ', payload)
+//   // Customize notification here
+//   const notificationTitle = payload.notification.title
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   }
+//   self.registration.showNotification(notificationTitle, notificationOptions)
+// })
+
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
+
+firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   appId: '1:79944665764:web:cc722d5d8f9ca080bfb431',
   projectId: 'eventful-870ba',
@@ -12,9 +34,9 @@ const app = initializeApp({
   messagingSenderId: '79944665764',
 })
 
-const messaging = getMessaging(app)
+const messaging = firebase.messaging()
 
-onBackgroundMessage(messaging, (payload) => {
+messaging.onBackgroundMessage((payload) => {
   console.log('Received background message ', payload)
   // Customize notification here
   const notificationTitle = payload.notification.title
