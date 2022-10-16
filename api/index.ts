6fa3aac86d0940e1d1d8e142cec45cd3f7bae4e6
xharris <xhh950@gmail.com>
2022-10-16 13:01:58 -0400

    bugfixes after ping changes

--- api/index.ts
+++ api/index.ts
@@ -22,6 +22,7 @@ const allowedOrigins = [
   'capacitor://localhost',
   'ionic://localhost',
   'http://localhost',
+  'http://localhost:3001',
   'http://localhost:8080',
   'http://localhost:8100',
 ]
--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,1 +1,4 @@
-export default process.env ?? {}
+export const IS_MOBILE = process.env.IS_MOBILE
+export const NODE_ENV = process.env.NODE_ENV
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL
+export const REACT_APP_SOCKET_URL = process.env.REACT_APP_SOCKET_URL
